3f69f54d5483b51253ab6b228639a3b05ab1385e
Brian Whicheloe <brian@snapshot.is>
2014-06-24 13:53:03 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<link type="text/css" href="assets/css/style.css" rel="stylesheet" />
+</head>
+<body>
+
+	<div class="page_title">
+		<span class="zimm">ZIMM</span><span class="eloe">eloe</span>
+	</div>
+
+	<div class="page_title_2">
+		<span class="zimm">Zimm</span><span class="eloe">eloe</span>
+	</div>
+
+	<div class="page_title_3">
+		<span class="zimm">Zimm</span><span class="eloe">eloe</span>
+	</div>
+
+</body>
+</html>
